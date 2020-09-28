8ef47c87ce02acfa105415cad081d85c969b6fc3
root <nikhilbhide15@gmail.com>
2020-09-28 20:15:08 +0530

    integrated amplify with reactjs

---
+++ src/components/DisplayPosts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { listPosts } from '../graphql/queries'
+import { API,graphqloperation } from 'aws-amplify'
+
+
+class DisplayPosts extends Component {
+    render(){
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
+
+export default DisplayPosts;
